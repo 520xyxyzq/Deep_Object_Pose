69f3b0d639ae261b65e5d86f90c138df5a15b018
Ziqi Lu <54910634+520xyxyzq@users.noreply.github.com>
2021-05-10 14:29:22 -0400

    add cards_evaluate

---
+++ scripts/cards_evaluate.py
@@ -0,0 +1,213 @@
+#! /usr/bin/env python
+
+'''
+evaluate.py: script to evaluate dope cards detector
+             including mis-classification rate & pose estimation accuracy
+NOTE:             
+We need to keep rosbag, apriltag_ros and dope/feature_based_pose_est running
+before running this script
+'''
+
+__author__ = "Ziqi Lu, Violet Killy"
+__email__ = 'ziqilu@mit.edu, violetk@mit.edu'
+__copyright__ = "Copyright 2021 The Ambitious Folks of the MRG"
+
+import rospy
+import cv2
+import numpy as np
+import tf
+
+from apriltag_ros.msg import AprilTagDetectionArray # for card/tag poses
+from vision_msgs.msg import Detection3DArray # for dope poses
+
+from scipy.spatial.transform import Rotation as Rot
+
+class CardsEvaluate(object):
+
+    def __init__(self):
+
+        self.tag_size, self.card_size = 0.05, [0.057, 0.089]
+        # Size of the interior square bounded by the tag corners
+        self.square_size = 0.1
+        # Tag ID (in /tag_detections msg published by apriltag_ros)
+        self.tags = np.array([0, 1, 2, 3])
+        # Relative translations of test card CENTER wrt tags 
+        self.card2tags = np.array([
+            [ (self.tag_size+self.square_size)/2, 
+              (self.tag_size+self.square_size)/2, 0],
+            [-(self.tag_size+self.square_size)/2, 
+              (self.tag_size+self.square_size)/2, 0],
+            [ (self.tag_size+self.square_size)/2,
+             -(self.tag_size+self.square_size)/2, 0],
+            [-(self.tag_size+self.square_size)/2,
+             -(self.tag_size+self.square_size)/2, 0]
+            ])
+        # Subscribe to tags' id-poses 
+        rospy.Subscriber("/tag_detections", AprilTagDetectionArray, self.tag_det_cb)
+        # Subscriber for feature based card pose estimator
+        rospy.Subscriber('/card_detections', AprilTagDetectionArray, self.card_det_cb)
+        #TODO: make a dope subscriber
+
+        # Counters:
+        self.inlier, self.outlier, self.dets = 0, 0, 0
+        # Translation & rotation distances
+        self.dist_rot, self.dist_trans = [], []
+
+        self.ctrl_c = False
+        rospy.on_shutdown(self.shutdownhook)
+
+    def shutdownhook(self):
+        if not self.dets:
+            rospy.logwarn("No card detection!")
+        else:
+            rospy.loginfo("Number of detections: %d", self.dets)
+            rospy.loginfo("True positive rate: %.2f", 
+                self.inlier/np.double(self.dets))
+            rospy.loginfo("False positive rate: %.2f",
+                self.outlier/np.double(self.dets))
+            rospy.loginfo("Average translation distance: %.2f", 
+                sum(self.dist_trans)/(1e-20+len(self.dist_trans)))
+            rospy.loginfo("Average rotation distance: %.2f", 
+                sum(self.dist_rot)/(1e-20+len(self.dist_rot)))            
+        self.ctrl_c = True
+
+    def tag_det_cb(self, msg):
+        '''
+        Callback function for /tag_detections msg (from apriltag_ros)  
+        Input: 
+            msg(AprilTagDetectionArray msg)
+        '''
+        dets = msg.detections # detections msg
+        tag_trans = np.zeros((len(dets),3))
+        tag_quats = np.zeros((len(dets),4))
+        for ii in range(len(dets)):
+            # Relative orientation of tag
+            tag_quats[ii,:] = np.array([dets[ii].pose.pose.pose.orientation.x,
+                                        dets[ii].pose.pose.pose.orientation.y,
+                                        dets[ii].pose.pose.pose.orientation.z,
+                                        dets[ii].pose.pose.pose.orientation.w])
+            card2tag = self.card2tags[self.tags==dets[ii].id[0]].ravel()
+            # Relative position of tag center
+            tag_trans[ii,:] = np.array([dets[ii].pose.pose.pose.position.x,
+                                        dets[ii].pose.pose.pose.position.y,
+                                        dets[ii].pose.pose.pose.position.z])\
+                            + Rot.from_quat(tag_quats[ii,:]).as_dcm()\
+                            .dot(card2tag) 
+
+        if dets:    
+            self.tag_trans = np.mean(tag_trans, 0)
+            # This is not rigorous but work as orientations are very similar
+            # TODO(ziqi): make orientation avg rigorous
+            self.tag_quat = np.mean(tag_quats, 0)
+            # Transform tag's coordinate system to card's (180deg roll flip)
+            self.tag_quat = self.quat_roll_flip(self.tag_quat)
+
+        # If yaw angle is not within [-90, 90] deg, map the angle to within it   
+        rpy = Rot.from_quat(self.tag_quat).as_euler("xyz", degrees=True)    
+        if rpy[-1] > 90 or rpy[-1] < -90:
+            self.tag_quat = self.quat_yaw_flip(self.tag_quat)
+        # Uncomment to debug
+        #rospy.loginfo("Average tag translation: %s", str(self.tag_trans))       
+        #rospy.loginfo("Average tag quaternion: %s", str(self.tag_quat))  
+
+    def card_det_cb(self, msg):
+        '''
+        Callback function for /card_detections msg (feature based method)  
+        Input: 
+            msg(AprilTagDetectionArray msg)
+        '''
+        # Query tag pose first to minimize lag
+        tag_trans, tag_quat = self.tag_trans, self.tag_quat
+        # Increment #detections
+        self.dets += 1
+        # card detection
+        dets = msg.detections
+        # It's possible but rare to have 2 detections in the same frame
+        # TODO(ziqi): deal with this situation
+        if len(dets) > 1: rospy.logwarn("More than 1 card detected")
+        else:
+            card_quat = np.array([dets[0].pose.pose.pose.orientation.x,
+                                   dets[0].pose.pose.pose.orientation.y,
+                                   dets[0].pose.pose.pose.orientation.z,
+                                   dets[0].pose.pose.pose.orientation.w])
+            # card coordinate from corner to center 
+            card_trans = np.array([dets[0].pose.pose.pose.position.x,
+                                   dets[0].pose.pose.pose.position.y,
+                                   dets[0].pose.pose.pose.position.z])\
+                        + Rot.from_quat(card_quat).as_dcm()\
+                        .dot(np.array(self.card_size+[0.0])/2)
+
+            # Compute the distance to ground truth
+            dist_trans = np.linalg.norm(card_trans - tag_trans)
+            dist_rot = self.computeRotDist(card_quat, tag_quat) 
+            # Treat as outlier as relative translation > square size
+            if dist_trans > self.square_size:
+                self.outlier += 1
+            else:
+                self.inlier += 1
+                self.dist_trans.append(dist_trans)
+                self.dist_rot.append(dist_rot)
+
+    def dope_det_cb(self, msg):
+        '''
+        Callback function for /... msg (dope published)  
+        Input: 
+            msg(... msg)
+        '''
+        # TODO: implement this
+
+    def computeRotDist(self, quat1, quat2):
+        '''
+        Util function to compute the chordal distance between two quaterninons
+        Input:
+            quat1(4-array)
+            quat2(4-array)
+        Output:
+            dist(double)
+        '''
+        assert len(quat1) == 4&len(quat2) == 4
+        rotmat1 = Rot.from_quat(quat1).as_dcm()
+        rotmat2 = Rot.from_quat(quat2).as_dcm()
+        return np.linalg.norm(rotmat1-rotmat2)
+
+    def quat_roll_flip(self, quat_in):
+        '''
+        Util function to flip a quaternion's yaw angle by 180deg  
+        Input:
+            quat_in (4-array) 
+        Output:
+            quat_out (4-array)
+        '''
+        assert len(quat_in) == 4
+        rotmat = Rot.from_quat(quat_in).as_dcm()
+        trans_matrix = np.eye(3)
+        trans_matrix[1,1], trans_matrix[2,2] = -1.0, -1.0
+        return Rot.from_dcm(rotmat.dot(trans_matrix)).as_quat()        
+
+    def quat_yaw_flip(self, quat_in):
+        '''
+        Util function to flip a quaternion's yaw angle by 180deg  
+        Input:
+            quat_in (4-array) 
+        Output:
+            quat_out (4-array)
+        '''
+        assert len(quat_in) == 4
+        rotmat = Rot.from_quat(quat_in).as_dcm()
+        trans_matrix = np.eye(3)
+        trans_matrix[1,1], trans_matrix[0,0] = -1.0, -1.0
+        return Rot.from_dcm(rotmat.dot(trans_matrix)).as_quat()
+
+    def main(self):
+        while not self.ctrl_c:
+            continue
+
+if __name__ == "__main__":
+    rospy.init_node("cards_evaluate_node", anonymous = True)
+    cards_evaluate = CardsEvaluate()
+    try:
+        cards_evaluate.main()
+    except rospy.ROSInterruptException:
+        pass
+
+    
