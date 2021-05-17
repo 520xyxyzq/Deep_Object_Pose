efd193e5513a1baa5192be2cdcd7ae3a03ea9cec
Ziqi Lu <54910634+520xyxyzq@users.noreply.github.com>
2021-05-17 11:19:27 -0400

    fix tag_pose bug

--- scripts/cards_evaluate.py
+++ scripts/cards_evaluate.py
@@ -3,9 +3,6 @@
 '''
 evaluate.py: script to evaluate dope cards detector
              including mis-classification rate & pose estimation accuracy
-NOTE:             
-We need to keep rosbag, apriltag_ros and dope/feature_based_pose_est running
-before running this script
 '''
 
 __author__ = "Ziqi Lu, Violet Killy"
@@ -15,11 +12,9 @@ __copyright__ = "Copyright 2021 The Ambitious Folks of the MRG"
 import rospy
 import cv2
 import numpy as np
-import os
 
 from apriltag_ros.msg import AprilTagDetectionArray # for card/tag poses
 from vision_msgs.msg import Detection3DArray # for dope poses
-from sensor_msgs.msg import Image
 
 from scipy.spatial.transform import Rotation as Rot
 
@@ -47,7 +42,8 @@ class CardsEvaluate(object):
         rospy.Subscriber("/tag_detections", AprilTagDetectionArray, self.tag_det_cb)
         # Subscriber for feature based card pose estimator
         rospy.Subscriber('/card_detections', AprilTagDetectionArray, self.card_det_cb)
-        #TODO: make a dope subscriber
+        #Subscriber for dope poses
+        rospy.Subscriber('/dope/detected_objects', Detection3DArray, self.dope_det_cb)
 
         # Counters:
         self.inlier, self.outlier, self.dets = 0, 0, 0
@@ -99,14 +95,16 @@ class CardsEvaluate(object):
             self.tag_trans = np.mean(tag_trans, 0)
             # This is not rigorous but work as orientations are very similar
             # TODO(ziqi): make orientation avg rigorous
-            self.tag_quat = np.mean(tag_quats, 0)
+            tag_quat = np.mean(tag_quats, 0)
             # Transform tag's coordinate system to card's (180deg roll flip)
-            self.tag_quat = self.quat_roll_flip(self.tag_quat)
+            tag_quat = self.quat_roll_flip(tag_quat)
 
-        # If yaw angle is not within [-90, 90] deg, map the angle to within it   
-        rpy = Rot.from_quat(self.tag_quat).as_euler("xyz", degrees=True)    
-        if rpy[-1] > 90 or rpy[-1] < -90:
-            self.tag_quat = self.quat_yaw_flip(self.tag_quat)
+            # If yaw angle is not within [-90, 90] deg, map the angle to within it   
+            rpy = Rot.from_quat(tag_quat).as_euler("xyz", degrees=True)    
+            if rpy[-1] > 90 or rpy[-1] < -90:
+                self.tag_quat = self.quat_yaw_flip(tag_quat)
+            else:
+                self.tag_quat = tag_quat
         # Uncomment to debug
         #rospy.loginfo("Average tag translation: %s", str(self.tag_trans))       
         #rospy.loginfo("Average tag quaternion: %s", str(self.tag_quat))  
@@ -125,7 +123,7 @@ class CardsEvaluate(object):
         dets = msg.detections
         # It's possible but rare to have 2 detections in the same frame
         # TODO(ziqi): deal with this situation
-        if len(dets) > 1: rospy.logwarn("More than 1 card detected")
+        if len(dets) > 2: rospy.logwarn("More than 1 card detected")
         elif len(dets) == 0:
             rospy.logdebug("No card detected")
         else:
@@ -155,17 +153,58 @@ class CardsEvaluate(object):
 
     def dope_det_cb(self, msg):
         '''
-        Callback function for /... msg (dope published)  
+        Callback function for /dope/detected_objects msg (dope published)  
         Input: 
-            msg(... msg)
+            msg(Detection3DArray msg)
         '''
-        # TODO: implement 
-        # Most of this function should be exactly the same as card_test_cb
-        # NOTE: I'm not 100% sure if the card's relative yaw angle is within [-90, 90]
-        #       It might be [-180, 0], [0, 180], etc.
-        #       So we should check that and might need to map the rotation back to [-90, 90]
-        
+         
+        # Query tag pose first to minimize lag
+        tag_trans, tag_quat = self.tag_trans, self.tag_quat
+        align = np.array([[0,1,0], [0,0,-1], [-1, 0, 0]])
+        # Increment #detections
+        self.dets += 1
+        # dope detection
+        dets = msg.detections
+        if len(dets) > 1: rospy.logwarn("More than 1 card detected")
+        elif len(dets) == 0:
+            rospy.logdebug("No card detected")
+        else:
+            dope_quat =np.array([dets[0].results[0].pose.pose.orientation.x,
+                                       dets[0].results[0].pose.pose.orientation.y,
+                                       dets[0].results[0].pose.pose.orientation.z,
+                                       dets[0].results[0].pose.pose.orientation.w])
+            
+            Rm = Rot.from_quat(dope_quat).as_dcm() 
+            dope_quat = Rot.from_dcm(Rm.dot(align)).as_quat()
+            
+            # card coordinate from corner to center 
+            dope_trans = np.array([dets[0].results[0].pose.pose.position.x,
+                                       dets[0].results[0].pose.pose.position.y,
+                                       dets[0].results[0].pose.pose.position.z])           
+                    
+            # Compute the distance to ground truth
+            rpy = Rot.from_quat(dope_quat).as_euler("xyz", degrees=True)    
+            if rpy[-1] > 90 or rpy[-1] < -90:
+                dope_quat = self.quat_yaw_flip(dope_quat)      
 
+            dist_trans = np.linalg.norm(dope_trans - tag_trans)
+            dist_rot = self.computeRotDist(dope_quat, tag_quat) 
+            
+            rpy1 = Rot.from_quat(dope_quat).as_euler("xyz", degrees=True)[-1]
+            rpy2 = Rot.from_quat(tag_quat).as_euler("xyz", degrees=True)[-1]
+            #print(rpy1,rpy2 )
+            if dist_rot>1.5 and abs(rpy1-rpy2)<5: 
+                rospy.logwarn("%.4f>1.5"%dist_rot)
+                print(dope_quat, tag_quat)
+            # Treat detection as outlier if relative translation > threshold
+            # NOTE: we may need to tune the threshold 
+            # or make more advanced criteria to correctly separate outliers
+            if dist_trans > self.square_size:
+                self.outlier += 1
+            else:
+                self.inlier += 1
+                self.dist_trans.append(dist_trans)
+                self.dist_rot.append(dist_rot)
 
     def computeRotDist(self, quat1, quat2):
         '''
@@ -177,6 +216,10 @@ class CardsEvaluate(object):
             dist(double)
         '''
         assert len(quat1) == 4 & len(quat2) == 4
+        rpy1 = Rot.from_quat(quat1).as_euler("xyz", degrees=True) 
+        rpy2 = Rot.from_quat(quat2).as_euler("xyz", degrees=True) 
+        if rpy1[-1]*rpy2[-1]<0 and abs(rpy1[-1]-90)<20 and (rpy2[-1]-90)<20:
+            quat1 = self.quat_yaw_flip(quat1)
         rotmat1 = Rot.from_quat(quat1).as_dcm()
         rotmat2 = Rot.from_quat(quat2).as_dcm()
         return np.linalg.norm(rotmat1-rotmat2)
